b308e12c89aad10a099c762f1f786a420c8b9c5e
maxlink <max.durham.link@gmail.com>
2024-04-04 19:35:50 -0500

    working on win state logic with col_checker and row_checker. Col checker almost works but is comparing rows to columns accidentally. Row_checker is broken and needs to reworked to find rows

--- ab.py
+++ ab.py
@@ -1,5 +1,8 @@
 import numpy as np 
 
+# import sys for early exiting when game won 
+import sys 
+
 def board(): 
     # board_size = input("Enter board size(row, col): ")
     board_size = "4, 4" 
@@ -69,34 +72,66 @@ def turn(player, board):
     print("new board:\n", new_board) 
     return new_board
 
-def state_check(state): 
-    # print("State:" , state) 
-    # check if the game is won 
-    # analyze all rows for four 1 or -1 
-    # analyze all columns for four 1 or -1 
-    # how do we do diagonals? start on a row and move in until you find 1 or -1
-    # finding 1 or -1 determines which should be checked, then go to the next row 
-    # on the next row, go one further than you went on the first row.... repeat until 4 hit 
-    n = len(state) 
-    for col in range(n): 
-        for row in range(n):
-            element = state[col][row] 
-            element2 = state[row][col]
-            # print("s1: ", element)
-            # print("s2: ", element2) 
+def row_checker(board): # broken 
+    # initialize state to 0 because the game by default is not won 
+    state = 0 
+    # get the length of the board
+    # for 4x4 should be 4 becauase len takes the vertical length 
+    n = len(board)
+    
+    # generate comparison array of 1's 
+    # dynamic array allocation with n 
+    # win state changes based on player 
+    # n is the amount of columns, filling each column with only a single 1 to make a 1 row 
+    win_state1 = np.ones((1, n), dtype = int) 
+     # negative win state 
+    win_state2 = np.full((1, n), -1) 
+ 
+    # check every row of the board 
+    # 0-3 range for row
+    for col in range(len(board)):
+        print("col for column function: \n", board[col][0])
 
-    '''
-    # grab every column, in the case of a 4x4, 0-3 
-    for col in range(n): 
-        for row in range(n):
-            # if -1 or 1 are found on the board
-            # checks down, so this checks the columns.... 
-            if(state[col][row] == -1 or state[col][row] == 1): 
-                print("checking all columns") 
+        print("col win state: ", win_state1) 
+        # print(board[col][0])
+        # use np.array_equal to compare arrays 
+        if(np.array_equal(board[col][0], win_state1) or np.array_equal(board[col][0], win_state2)): 
+           state = 1  
+    return state 
+
+# finds column 
+# need to rename 
+def col_checker(board):
+
+    # real logic 
+    # initialize state to 0 because the game by default is not won 
+    state = 0 
+    # get the length of the board
+    # for 4x4 should be 4 becauase len takes the vertical length 
+    n = len(board)
+    
+    # generate comparison array of 1's 
+    # n is for dynamic array of 1 allocation based on board size 
+    # n is the amount of columns, filling them with 1's 
+    win_state1 = np.ones((1, n), dtype = int)
+    # negative win state 
+    # np.full for general fill 
+    win_state2 = np.full((1, n), -1) 
+    # print("win_state:", win_state) 
+    # check every row of the board 
+    # 0-3 range for row 
+    for row in range(n): 
+        print("this is the row being compared: \n", board[row])
+        print("win state:\n ", win_state1)
+        # if the game is won, set state to 1 
+        # compare using np.array_equal to compare each element of the arrays to each other 
+        # if they are equal enter the if statement 
+        # this may not be okay to compare arrays directly. 
+        if(np.array_equal(board[row], win_state1) or np.array_equal(board[row], win_state2)): 
+            state = 1 
         
-            if(state[row][col] == -1 or state[row][col] == 1): 
-                print("checking all rows") 
-        '''
+
+    return state 
 
 def play(player, board): 
     if(player == 1): 
@@ -126,9 +161,28 @@ def play(player, board):
     print("Updated board: \n", board)
     return board 
 
-# testing 
 
-# create initial state 
+def state_check(board, player): 
+    # print("State:" , state) 
+    # check if the game is won 
+    # analyze all rows for four 1 or -1 
+    # analyze all columns for four 1 or -1 
+    # how do we do diagonals? start on a row and move in until you find 1 or -1
+    # finding 1 or -1 determines which should be checked, then go to the next row 
+    # on the next row, go one further than you went on the first row.... repeat until 4 hit
+    # check rows 
+    state_row = row_checker(board) 
+    # check columns 
+    state_col = col_checker(board)
+    # check diagonal 
+
+    if(state_row == 1 or state_col == 1): 
+        print("player: ", player, " won! Ending game.")
+        # exit the program because the game has been won 
+        sys.exit()
+    else: 
+        print("win state not reached") 
+# test 
 board = board();
 print("board:\n", board)
 
@@ -143,33 +197,28 @@ while True:
     # red goes first 
     player = red 
     board = play(player, board)
+    
+    # check state - state_check will end the game if a disc is in four adjacent spots 
+    state_check(board, player)
 
     # yellow goes second 
     player = yellow 
     board = play(player, board)
+
+    # check state - state_check will end the game if a disc is in four adjacent spots 
+    state_check(board, player) 
     
     # for testing break 
-    if(count == 3): 
+    if(count == 5): 
         break; 
     
     # increment count 
     count+=1 
+# test win condition here
+player = 1 
+test_win = [[1, 0, 0, 0], 
+            [1, 0, 0, 0]]
+print("testing win state!")  
+state_check(test_win, player) 
 
 
-
-''' old imp 
-min_max(); 
-alpha_beta(); 
-red = 1 
-new_board = turn(red, board);
-
-yellow = -1 
-new_board = turn(yellow, new_board); 
-
-state_check(new_board) 
-
-# win_test = [1, 1, 1, 1] 
-
-# state_check(win_test) 
-
-'''
